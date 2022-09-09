f5d4b4fb632401fbf2353a50746112930186615c
frank <f.alkema@mobiel.nl>
2022-09-09 13:37:03 +0200

    finished the worlde game

--- wordle.exs
+++ wordle.exs
@@ -6,19 +6,49 @@ defmodule Wordle do
     random_word
   end
 
-  def guess_wordletter(word, letter) do
-    if String.contains?(word, letter) do
+  def guess_word(guess, word) do
+    if String.contains?(guess, word) do
       IO.puts "You guessed correctly!"
     else
       IO.puts "You guessed incorrectly!"
+      check_letters(guess, word)
+      collect_and_check_word(word)
     end
   end
 
+  def collect_and_check_word(word) do
+    IO.puts "Guess the word!"
+    guess = IO.gets ">"
+    guess_word(guess, word)
+  end
+
+  def check_letters(guess, word) do
+    guess_list = String.split(guess, "",trim: true)
+    word_list = String.split(word, "",trim: true)
+
+    Enum.each(guess_list, fn letter ->
+      if letter in word_list do
+        IO.puts "You guessed #{letter} correctly!"
+      end
+    end)
+  end
+
+
+  def play do
+     word = random_word()
+      IO.puts "Guess the word!"
+      guess = IO.gets ">"
+      guess_word(guess, word)
+    end
 end
 
+Wordle.play
 
 
-guessed_word = IO.gets "Guess a letter: \n"
 
 
-guessed_word |> String.trim |> S
+
+
+
+
+
